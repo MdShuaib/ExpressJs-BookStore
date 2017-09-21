564b87888648931bf76c1cc37c3a597552c5ff57
Vedprakash arya <vedprakash@kwd.co.in>
2017-09-21 12:17:08 +0530

    updated with api/genres routes

--- server.js
+++ server.js
@@ -2,13 +2,23 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-
+Genre =  require("./models/genre");
 // Connect to Mongoose
 mongoose.connect('mongodb://localhost/bookstore');
 var db = mongoose.connection;
 
 app.get('/', (req, res) => {
 	res.send('Hello Api');
+});
+
+app.get('/api/genres', (req, res) => {
+	Genre.getGenres(function(err, genres) {
+		if(err) {
+			throw err;
+			console.log(err)
+		}
+		res.json(genres);
+	});
 });
 
 app.listen(3000);
