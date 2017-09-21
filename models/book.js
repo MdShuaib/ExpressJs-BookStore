1a65e698effd3aa32b8726ef4c97efd0461db03e
Vedprakash arya <vedprakash@kwd.co.in>
2017-09-21 12:27:22 +0530

    added schema for book model

---
+++ models/book.js
@@ -0,0 +1,42 @@
+var mongoose = require('mongoose');
+
+// Book Schema
+var bookSchema = mongoose.Schema({
+	title:{
+		type: String,
+		required: true
+	},
+	genre:{
+		type:String,
+		required:true
+	},
+	description:{
+		type:String,
+		required:false
+	},
+	author:{
+		type:String,
+		required:true
+	},
+	publisher:{
+		type:String,
+		required:true
+	},
+	image_url:{
+		type:String
+	},
+	buy_url:{
+		type:String
+	},
+	create_date:{
+		type: Date,
+		default: Date.now
+	}
+});
+
+var Book = module.exports = mongoose.model('Book', bookSchema);
+
+// Get Books
+module.exports.getBooks = (callback, limit) => {
+	Book.find(callback).limit(limit);
+}
